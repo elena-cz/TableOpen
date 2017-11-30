af3db275bbc3c293efad23d9ab628e4e4dea83fe
Luis Alvarez <lalvarez820@gmail.com>
2017-11-30 10:08:34 -0800

    'refactored database'

--- database/models/Customers.js
+++ database/models/Customers.js
@@ -1,9 +1,13 @@
 const bookshelf = require('../index').bookshelf;
+const Reservation = require('./Reservations');
 
 const Customer = bookshelf.Model.extend({
   tableName: 'customers',
   byId(id) {
     return this.forge().query({ where: { id } }).fetch();
+  },
+  reservation() {
+    return this.belongsTo('Reservation');
   },
 });
 
